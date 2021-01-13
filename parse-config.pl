e4ec4ad992d9e63e7e536a9d67033d727cf314b0
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-13 14:28:35 +0100

    draft printing sub

--- parse-config.pl
+++ parse-config.pl
@@ -25,8 +25,11 @@ my $data_string = get ( $data_url)  or die " cannot retrieve boiler data $data_u
 my @data = split ( '\n', $data_string ) ;
 printf "data: %s\n", join ( ' : ' , @data );
 
+my $printf_fmt = "id=%03d,  (   %10s   ) ,  %s %s , %s,   %s\n";
 
 for my $key (sort numeric_sort   keys %config) {
+	print_config_item ( $data[ $key ]  , $config{ $key } , $printf_fmt );
+  if (0) {
 	my $item_desc = $config{ $key };
 	my $unit = $item_desc->{ unit } ;
 	$unit = '' unless  defined $unit ;
@@ -44,7 +47,7 @@ for my $key (sort numeric_sort   keys %config) {
 		$item_desc->{ type } ,
 		$item_desc->{ name } ,
 		;
-
+  }
 }
 
 print "\n" ;
@@ -61,6 +64,34 @@ print "\n" ;
 exit;
 
 #------------------------------------------
-# sort helper
-sub numeric_sort { $a <=> $b  } 
+
+# print_config_item ( $dat_val, $item, $formatf )
+sub print_config_item {
+	my ( $dat_val, $item_desc, $formatf ) = @_;
+
+	# my $item_desc = $config{ $key };
+	my $unit = $item_desc->{ unit } ;
+	$unit = '' unless  defined $unit ;
+
+	my $tag = $item_desc->{ tag } ;
+	$tag = '' unless  defined $tag ;
+
+
+	# print $key, ' - ' ;
+	# printf "id=%03d,  (   %10s   ) ,  %s %s , %s,   %s\n",
+	printf $formatf ,
+		$item_desc->{ id } ,
+		$tag,
+		# $data[ $key ] ,
+		$dat_val,
+		$unit ,			# $item_desc->{ unit } ,
+		$item_desc->{ type } ,
+		$item_desc->{ name } ,
+		;
+
+
+
+}
+
+
 
