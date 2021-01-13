0715361b640a2a855707f9233c84882c6bfe0c93
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-13 12:40:01 +0100

    ... cleanup ....

--- parse-config.pl
+++ parse-config.pl
@@ -13,17 +13,8 @@ our $desc_url, $data_url;
 our %credentials;
 require ('./config.pm');
 
-# our %credentials;
-# require ('./secret.pm');
-# my $data_url =  'http://' . $credentials{ host } ;
-# $data_url .=  $credentials{ data_api } ;
-# $data_url .= '?key=' . $credentials{ key } ;
-
 
 print Dumper (%config,  %credentials, $desc_url, $data_url);
-
-# print $desc_url;
-# print $data_url, "\n" ;
 
 my $data = get ( $data_url)  or die " cannot retrieve boiler data $data_url";
 print $data, "\n" ;
