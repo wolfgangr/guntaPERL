650a9fce8c87fb5a76b1d95ecdae6724e7d3439e
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-13 11:38:29 +0100

    add parse-config.pl

---
+++ parse-config.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+# read config.pm and output debug and devel info
+# run after every config change
+
+
+
+use Data::Dumper::Simple;
+use LWP::Simple;
+
+my $cfg_txt = read_file ('config.pm');
+print $cfg_txt;
+
+my $cfg_ptr = eval $cfg_txt;
+my %config = %$cfg_ptr ;
+print Dumper (%config);
+
+exit;
+
+#------------------------------------------
+
+sub read_file {
+	my $filename = shift;
+	my $content = `cat $filename`;
+	return $content ;
+}
