733f0539f7ead8828636fa5eefdfbecd6f2412a0
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-13 12:08:34 +0100

    retrieve data by confgd credentials

--- parse-config.pl
+++ parse-config.pl
@@ -13,9 +13,19 @@ require ('./config.pm');
 
 our %credentials;
 require ('./secret.pm');
+my $data_url =  'http://' . $credentials{ host } ;
+$data_url .=  $credentials{ data_api } ;
+$data_url .= '?key=' . $credentials{ key } ;
 
 
 print Dumper (%config,  %credentials);
+
+
+print $data_url, "\n" ;
+my $data = get ( $data_url)  or die " cannot retrieve boiler data $data_url";
+print $data, "\n" ;
+
+
 
 exit;
 
