fa2c089912d706792933a3e810b374b7f83afa57
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-13 12:47:24 +0100

    nice print of data

--- parse-config.pl
+++ parse-config.pl
@@ -2,6 +2,8 @@
 # read config.pm and output debug and devel info
 # run after every config change
 
+use warnings;
+use strict;
 
 
 use Data::Dumper::Simple;
@@ -9,17 +11,18 @@ use LWP::Simple;
 
 
 our %config;
-our $desc_url, $data_url;
+our ($desc_url, $data_url);
 our %credentials;
 require ('./config.pm');
 
 
 print Dumper (%config,  %credentials, $desc_url, $data_url);
 
-my $data = get ( $data_url)  or die " cannot retrieve boiler data $data_url";
-print $data, "\n" ;
+my $data_string = get ( $data_url)  or die " cannot retrieve boiler data $data_url";
+# print $data_string, "\n" ;
 
-
+my @data = split ( '\n', $data_string ) ;
+printf "data: %s\n", join ( ' : ' , @data );
 
 exit;
 
