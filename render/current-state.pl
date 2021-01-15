0c0e33088f29625d052c78c87387dc65f513b21b
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-16 00:17:27 +0100

    boilerplating

---
+++ render/current-state.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI qw/:standard/;
+use Data::Dumper ;
+use Storable ();
+
+our $dtformat = '+\'%d.%m.%Y %T\'' ; # datetime format string for console `date`
+our $RRDdtf = "+\'%d.%m.%Y %H:%M\'" ; # RRD does not like seconds here 
+
+
+print header();
+print start_html(-title => 'Testpage No2');
+print h1('Test 2');
+print end_html();
+
+exit ;
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# debug with continued laoding
+sub debug {
+  print
+    "\n<pre><code>\n",
+    Dumper ( @_),
+    "\</code></pre>\n",
+  ;
+}
+
+# final die like debug
+sub DEBUG {
+    print header,
+    start_html('### DEBUG ###'),
+    debug ( @_) ,
+    end_html
+  ;
+  exit; # is it bad habit to exit from a sum??
+}
+
+sub mydatetime {
+  my $arg = shift;
+  my $dtf = shift;
+  $dtf = $dtformat unless $dtf ;
+  my $rv =`date -d \@$arg $dtf` ;
+  chomp $rv;
+  return $rv ;
+}
+
+sub rrddatetime {
+  my $arg = shift;
+  return mydatetime ($arg , $RRDdtf) ;
+}
+
