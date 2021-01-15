9ef144a7d6da58db7c15e65942db030632c795f6
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-16 00:48:00 +0100

    ...

--- render/current-state.pl
+++ render/current-state.pl
@@ -8,10 +8,26 @@ use Time::Piece;
 use File::stat;
 
 
-our $dtformat = '+\'%d.%m.%Y %T\'' ; # datetime format string for console `date`
-our $RRDdtf = "+\'%d.%m.%Y %H:%M\'" ; # RRD does not like seconds here 
+# our $dtformat = '+\'%d.%m.%Y %T\'' ; # datetime format string for console `date`
+# our $RRDdtf = "+\'%d.%m.%Y %H:%M\'" ; # RRD does not like seconds here 
 
 my $statusfile = '../rrd/tv_export.storable';
+
+# ~~~~ local config stuff 
+
+our (%config, %selectors, %config_by_tag );
+# our ($desc_url, $data_url);
+# our %credentials;
+require ('../config_plain.pm');
+
+# require ('./library.pm');
+
+# ~~~~~ end of config, start processing
+
+
+
+
+
 
 my $f_st = stat($statusfile) or die "No $statusfile: $!";
 my $sf_modified = $f_st->mtime ;
@@ -21,6 +37,12 @@ my $hr_modified = $tp_modified->ymd . ' - ' . $tp_modified->hms;
 
 my $tv_p = Storable::lock_retrieve( $statusfile );
 my %tv = %$tv_p ;
+
+
+
+
+
+# ~~~~~ end of processing, start HTML rendering ~~~~~~~~~~~~~~~
 
 print header();
 print start_html(-title => 'Testpage No2');
@@ -32,7 +54,7 @@ print end_html();
 
 exit ;
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~ subs below ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # debug with continued laoding
 sub debug {
@@ -53,17 +75,4 @@ sub DEBUG {
   exit; # is it bad habit to exit from a sum??
 }
 
-sub mydatetime {
-  my $arg = shift;
-  my $dtf = shift;
-  $dtf = $dtformat unless $dtf ;
-  my $rv =`date -d \@$arg $dtf` ;
-  chomp $rv;
-  return $rv ;
-}
-
-sub rrddatetime {
-  my $arg = shift;
-  return mydatetime ($arg , $RRDdtf) ;
-}
 
