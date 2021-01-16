6df9c154799d3c416b35bb5cf590adf44f310500
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-16 13:20:52 +0100

    process @ARGV

--- render/log-grep.pl
+++ render/log-grep.pl
@@ -16,15 +16,27 @@ chomp $cutcmd ;
 $cutcmd .= "  -d':' -f4  ";
 $cutcmd .= $logfile ;
 
+#~~~~ parse @ARGV
+
+my $from = shift @ARGV;
+my $until = shift @ARGV;
+die "usage: $0 fromtime untiltime { range | times } " unless scalar @ARGV;
+
+my $do_range = scalar grep { /range/ }  @ARGV;
+my $do_times = scalar grep { /times/ }  @ARGV;
+die "use either 'range'  'times' or both" unless ($do_range or  $do_times)  ;
+
+#~ do work ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 print STDERR $cutcmd , "\n"; 
 
 open (my $CUTTED, '-|', $cutcmd) or die " cannot open pipe $cutcmd";
 
 my $cnt=0;
+#~~~~~~ begin main loop
 while (<$CUTTED>) {
   $cnt++;
 }
-
+#~~~~~~ end main loop
 print $cnt , " lines found\n";
 
 close $CUTTED;
