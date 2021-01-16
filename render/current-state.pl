736edc6705da30bb70f0499144e42334b97a4710
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-16 01:24:44 +0100

    at lest it looks like a table on the web, and has values in it...

--- render/current-state.pl
+++ render/current-state.pl
@@ -7,22 +7,24 @@ use Storable ();
 use Time::Piece;
 use File::stat;
 
+# work around misconfiguration 
 chdir '..' ; # omG ;-\
 
 
 # our $dtformat = '+\'%d.%m.%Y %T\'' ; # datetime format string for console `date`
 # our $RRDdtf = "+\'%d.%m.%Y %H:%M\'" ; # RRD does not like seconds here 
 
-my $statusfile = '../rrd/tv_export.storable';
+my $statusfile = './rrd/tv_export.storable';
+my $title = "Guntamatic PowerChip aktueller Status";
 
 # ~~~~ local config stuff 
 
 our (%config, %selectors, %config_by_tag );
 # our ($desc_url, $data_url);
 # our %credentials;
-require ('../config_plain.pm');
+require ('./config_plain.pm');
 
-# require ('./library.pm');
+require ('./library.pm');
 
 # ~~~~~ end of config, start processing
 
@@ -40,18 +42,33 @@ my $hr_modified = $tp_modified->ymd . ' - ' . $tp_modified->hms;
 my $tv_p = Storable::lock_retrieve( $statusfile );
 my %tv = %$tv_p ;
 
+# my $printf_fmt = "id=%03d,  (   %10s   ) ,  %s %s , %s,   %s\n";
 
+my $printf_fmt = "<tr><td>%03d</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n";
+my $th  = "<table><tr><td>ID</td><td>K&uuml;rzel</td><td>Wert</td><td>Einheit</td><td>Typ</td><td>Beschreibung</td></tr>\n";
 
 
 
 # ~~~~~ end of processing, start HTML rendering ~~~~~~~~~~~~~~~
 
 print header();
-print start_html(-title => 'Testpage No2');
-print h1('Test 2');
+print start_html(-title => $title);
+print h1($title);
 
-debug ( [    \%tv, $sf_modified, $tp_modified , $hr_modified, \%config_by_tag ] ,
-       , [ qw(  *tv *sf_modified  *tp_modified   *hr_modified,  *config_by_tag    ) ] )	;
+print $th ;
+
+for my $id (sort numeric_sort   keys %config) {
+	my $tag = $config{ $id }->{ tag };
+	next unless $tag;
+	next unless defined  $tv{$tag};
+	print_config_item (  $tv{ $tag } , $config{ $id } , $printf_fmt );
+}
+
+
+print "</table>\n";
+
+debug ( [    \%tv, $sf_modified, $tp_modified , $hr_modified, \%config_by_tag , \%config ] ,
+       , [ qw(  *tv *sf_modified  *tp_modified   *hr_modified  *config_by_tag    *config   ) ] )	;
 
 print end_html();
 
