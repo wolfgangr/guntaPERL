486c2b4270c5e33a07c837ec24110f46174a4fec
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-16 14:18:20 +0100

    simple wrapper for tail / head to cut a range of lines from a file

---
+++ render/lines-range.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo file: $1 >&2
+echo from: $2 >&2
+echo lines $3 >&2
+
+if [ ! $3 ] 
+then 
+	echo "usage $0 filename start-line num-lines" >&2
+       	exit 
+fi
+
+echo "still alive" >&2
+tail -n+$2 $1 | head -n$3
+
