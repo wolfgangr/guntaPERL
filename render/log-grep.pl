5e730ba108eca26e04cf842018b1720deb2c6822
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-16 13:59:39 +0100

    log file grepper works as intended

--- render/log-grep.pl
+++ render/log-grep.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl
 #
 # grep log file by time and return either line range , time list  or line times
-# usage:
-# grep-log.pl fromtime untiltime { from | to | 
+# assume sorted entries
+#
+# intended consumer:
+# time tail -n+10000 ../olddata/daqdata.log.2 | head -n3
+#
 # by Wolfgang Rosner, 2021
 
 use warnings;
@@ -31,13 +34,26 @@ print STDERR $cutcmd , "\n";
 
 open (my $CUTTED, '-|', $cutcmd) or die " cannot open pipe $cutcmd";
 
-my $cnt=0;
+my $cnt_start=0;
+my $cnt_lines=0;
+ 
 #~~~~~~ begin main loop
+# be efficient, no sanity checking
 while (<$CUTTED>) {
-  $cnt++;
+
+	unless ($cnt_lines) {
+		$cnt_start++;
+		next if ($_ < $from);
+	}
+ 	last if ( $_ > $until) ; 
+	$cnt_lines++;
+
+	if ($do_times) { print $_ ; }
 }
 #~~~~~~ end main loop
-print $cnt , " lines found\n";
+# print $cnt , " lines found\n";
+if ($do_range) { printf "%s %s\n", $cnt_start, $cnt_lines }
+
 
 close $CUTTED;
 
