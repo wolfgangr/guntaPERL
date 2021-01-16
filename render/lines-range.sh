249aa1276dc78ed07797067d8453cc7316b5c66c
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-16 14:21:13 +0100

    comment out debugs

--- render/lines-range.sh
+++ render/lines-range.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo file: $1 >&2
-echo from: $2 >&2
-echo lines $3 >&2
+# echo file: $1 >&2
+# echo from: $2 >&2
+# echo lines $3 >&2
 
 if [ ! $3 ] 
 then 
 	echo "usage $0 filename start-line num-lines" >&2
-       	exit 
+      	exit 1
 fi
 
-echo "still alive" >&2
+# echo "still alive" >&2
 tail -n+$2 $1 | head -n$3
 
